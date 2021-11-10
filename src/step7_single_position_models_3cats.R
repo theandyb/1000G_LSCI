631265d6b9775a9d5d85a31cdc3e126756723466
Andy Beck <beckandy@umich.edu>
2021-11-10 16:07:51 -0500

    output excel files

--- src/step7_single_position_models_3cats.R
+++ src/step7_single_position_models_3cats.R
@@ -3,32 +3,32 @@
 library(tidyverse)
 library(foreach)
 library(doParallel)
-#library(writexl)
+library(writexl)
 
 #' Get the singleton counts for a sub-type at a flanking position
-#' 
+#'
 #' @param subtype One of the nine simple sub types
 #' @param rp Relative position which to get singleton counts from
 #' @param sp Super-population
 #' @return data frame with counts for each nucleotide
 get_singleton_pos <- function(subtype, rp, sp){
   nucs <- c("A", "C", "G", "T")
-  
+
   pos <- rp + 11
-  
+
   singleton_dir <- "/net/snowwhite/home/beckandy/research/1000G_LSCI/output/singletons/"
   fName <- paste0(singleton_dir, sp, "/", subtype, ".txt")
-  
+
   awk_cmd <- paste0("awk '{count[substr($1,", pos,", 1)]++}END{for(key in count) print(key, \"\\t\", count[key])}' ",
                     fName)
-  
+
   df <- vroom::vroom(pipe(awk_cmd), col_names = c("Nuc", "singletons"), delim = "\t", show_col_types = FALSE) %>%
     filter(Nuc %in% nucs )
   return(df)
 }
 
 #' Get nucleotide counts at position relative to control observation
-#' 
+#'
 #' @param subtype One of the 18 basic subtypes
 #' @param rp Position relative to focal site to get counts for
 #' @param sp Super-population
@@ -37,35 +37,35 @@ get_control_pos <- function(subtype, rp, sp){
 
   nucs <- c("A", "C", "G", "T")
   pos <- rp + 11
-  
+
   control_dir <- "/net/snowwhite/home/beckandy/research/1000G_LSCI/output/controls/"
   fName <- paste0(control_dir, sp, "/", subtype, ".txt")
-  
+
   awk_cmd <- paste0("awk '{count[substr($1,", pos,", 1)]++}END{for(key in count) print(key, \"\\t\", count[key])}' ",
                     fName)
-  
+
   df <- vroom::vroom(pipe(awk_cmd), col_names = c("Nuc", "controls"), delim = "\t", show_col_types = FALSE) %>%
     filter(Nuc %in% nucs)
   return(df)
 }
 
 #' Get genome-wide counts of nucleotides flanking A or C (3 category version)
-#' 
+#'
 #' @param nucleotide Either A, cpg, or non
 #' @param rp Flanking position at which we stratify the genome-wide count of A or C
 #' @return data.frame with genome-wide counts of nucleotides flanking focal site
 get_gw_position <- function(nucleotide, rp){
-  
+
   data_dir <- "/net/snowwhite/home/beckandy/research/1000G_LSCI/output/gw_1_count/3cat/"
   f_name <- paste0(data_dir, nucleotide, "_rp", rp, ".csv")
   df <- read_csv(f_name, col_types = cols())
   colnames(df) <- c("Nuc", "n_gw")
   return(df)
-  
+
 }
 
 #' Get singleton, control, and genome-wide counts for a sub-type at a flanking position
-#' 
+#'
 #' @param subtype One of the nine basic sub-types
 #' @param rp Flanking position at which we stratify the genome-wide count of A or C
 #' @param sp Super-population
@@ -78,11 +78,11 @@ get_all_position <- function(subtype, rp, sp){
   } else {
     nuc <- "cpg_C"
   }
-  
+
   df_s <- get_singleton_pos(subtype, rp, sp)
   df_c <- get_control_pos(subtype, rp, sp)
   df_gw <- get_gw_position(nuc, rp)
-  
+
   df <- full_join(df_s, df_c, by = "Nuc") %>%
     full_join(df_gw, by = "Nuc") %>%
     replace_na(list("singletons" = 0, "controls" = 0, "n_gw" = 0)) %>%
@@ -93,10 +93,10 @@ get_all_position <- function(subtype, rp, sp){
            exp_ct = sum(singletons) * pct_c) %>%
     mutate(chi_sq_gw = ((singletons - exp_gw)^2)/exp_gw,
            chi_sq_ct = ((singletons - exp_ct)^2)/exp_ct)
-  
+
   df$rp <- rp
   return(df)
-  
+
 }
 
 subtypes <- c("AT_CG", "AT_GC", "AT_TA",
@@ -116,7 +116,7 @@ for(sp in c("ALL", "AFR", "AMR", "EAS", "EUR", "SAS")){
     }
   }
   stopImplicitCluster()
-  
+
   out_dir <- paste0("/net/snowwhite/home/beckandy/research/1000G_LSCI/output/single_pos_df/", sp, "/")
   for(st in subtypes){
     message(st)
@@ -129,30 +129,31 @@ for(sp in c("ALL", "AFR", "AMR", "EAS", "EUR", "SAS")){
       write_csv(df2, out_file)
     }
   }
+  excel_results <- vector("list", length(subtypes))
+  names(excel_results) <- subtypes
+  for(st in subtypes){
+    df <- results[[st]]
+    df_f <- df %>%
+      filter(rp == -10) %>%
+      select(-rp) %>%
+      select(Nuc, singletons, controls, n_gw, chi_sq_gw, chi_sq_ct) %>%
+      gather(category, n, -Nuc)
+    colnames(df_f) <- c("Nuc", "category", "n-10")
+    for(i in c(-9:-1,1:10)){
+      df2 <- df %>%
+        filter(rp == i) %>%
+        select(-rp) %>%
+        select(Nuc, singletons, controls, n_gw, chi_sq_gw, chi_sq_ct) %>%
+        gather(category, n, -Nuc)
+      colnames(df2) <- c("Nuc", "category", paste0("n",i))
+      df_f <- full_join(df_f, df2, by = c("Nuc", "category")) %>%
+        mutate_all(~replace(., is.na(.), 0))
+    }
+    excel_results[[st]] <- df_f
+  }
+
+  write_xlsx(excel_results, paste0(out_dir, "1000G_", sp, ".xlsx"))
 }
 
-# excel_results <- vector("list", length(subtypes))
-# names(excel_results) <- subtypes
-# for(st in subtypes){
-#   df <- results[[st]]
-#   df_f <- df %>%
-#     filter(rp == i) %>%
-#     select(-rp) %>%
-#     select(Nuc, singletons, controls, n_gw, chi_sq_gw, chi_sq_ct) %>%
-#     gather(category, n, -Nuc)
-#   colnames(df_f) <- c("Nuc", "category", "n-10")
-#   for(i in c(-9:-1,1:10)){
-#     df2 <- df %>%
-#       filter(rp == i) %>%
-#       select(-rp) %>%
-#       select(Nuc, singletons, controls, n_gw, chi_sq_gw, chi_sq_ct) %>%
-#       gather(category, n, -Nuc)
-#     colnames(df2) <- c("Nuc", "category", paste0("n",i))
-#     df_f <- full_join(df_f, df2, by = c("Nuc", "category")) %>%
-#       mutate_all(~replace(., is.na(.), 0))
-#   }
-#   excel_results[[st]] <- df_f
-# }
-# 
-# write_xlsx(excel_results, paste0(out_dir, "all_sp_bridges.xlsx"))
+
 
